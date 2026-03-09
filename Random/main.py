dc279989bf518b714495afba97d90177d7566af7
Battleboy96 <tggblake.2012@gmail.com>
2026-03-09 12:17:22 +1000

    deleted:    App Folders/main.py 	modified:   Random/main.py

--- Random/main.py
+++ Random/main.py
@@ -1,18 +1,19 @@
+# Don't ask what this does! I have no idea and can't be bothered to read it.
 import sys
-import time as t
-import random as rand
+import time
+import random
 
 var1 = 1
 var2 = var1 + 1
 var3 = var2 * 2
-print(var3 + rand.randint(1, 10))
+print(var3 + random.randint(1, 10))
 
-rand_wait = rand.randint(1, 5)
+rand_wait = random.randint(1, 5)
 print(f"Waiting for {rand_wait} seconds...")
-t.sleep(rand_wait)
+time.sleep(rand_wait)
 print("Done waiting!")
 
-if var3 == rand.randint(5, 14):
+if var3 == random.randint(5, 14):
     print("Spontaneous combination!")
     sys.exit(0)
     
