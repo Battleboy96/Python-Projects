00c5d54299de522541601f5711bd8d4c9f23f627
Battleboy96 <tggblake.2012@gmail.com>
2026-05-28 17:40:40 +1000

    Remove comments and add exit prompt
    Removed redundant comments and added an exit prompt at the end of the game.

--- Number Guessing.py
+++ Number Guessing.py
@@ -1,21 +1,21 @@
 import random
 import time
 
-# Create number to guess between 1 and 10
+
 number = random.randint(1, 10)
 
-# Get user input as guess
+
 print("Guess a number between 1 and 10")
 guess = int(input())
 
-# Check if guess is correct
+
 if guess == number:
     print("You guessed the number!")
 
 else:
     print("Wrong guess, the number was", number)
-    # Tell user how far off they were
-    time.sleep(0.5) # Pause for visual appeal
+
+    time.sleep(0.5)
     print("You were off by", abs(guess - number))
 
-time.sleep(0.5)
+input("Press enter to exit")
